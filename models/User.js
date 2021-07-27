253046a009e3dc45ee0f34eccdb1a87983cd66b8
Alfonso Gonzalez <271396+a13z@users.noreply.github.com>
2021-07-27 22:24:45 +0200

    add uuid for user model. Email is not required

--- models/User.js
+++ models/User.js
@@ -1,13 +1,14 @@
 'use strict';
-
 /**
  * Lifecycle callbacks for the `User` model.
  */
 
+const uuid = require('uuid');
 const nonce = Math.floor(Math.random() * 10000);
 
 module.exports = {
   beforeCreate: async (model) => {
     model.set('nonce', nonce);
+    model.set('id', uuid());
   }
 };
